695c36c9d5fe57e07a6b80f9e0afea01e556594f
Karen <motorolochka@mail.ru>
2022-07-28 03:38:37 +0200

    calculation inputs added

--- js/main.js
+++ js/main.js
@@ -15,18 +15,23 @@ function screenHandler(){
         document.querySelector(".nav-menu").style.display = "flex";
         document.querySelector(".header-phone-block").style.display = "flex";
         document.querySelector(".context-menu").style.display = "flex";
-     
+
+        headerMenu.style.paddingTop = "0";
+        document.querySelector(".header-background").style.marginTop = "0";
+
         headerMenu.style.width = "100%";
+        headerMenu.style.paddingBottom = "0";
         logo.style.marginLeft = "130px";
         document.querySelector(".header-phone-block").style.paddingRight = "130px";
         document.querySelector(".header-phone-block").style.paddingTop = "20px";
-  
+        headerMenu.style.background = "linear-gradient(0deg, #F5F7F9, #F5F7F9), #ECE8E5";
     } else {
         navBar.style.display = "block";
         document.querySelector(".nav-menu").style.display = "none";
         document.querySelector(".header-phone-block").style.display = "none";
         document.querySelector(".context-menu").style.display = "none";
-  
+        headerMenu.style.paddingBottom = "20px";
+        document.querySelector(".header-background").style.marginTop = "0";
         logo.style.marginLeft = "20px";
       
     }
@@ -40,10 +45,11 @@ function mouseHandler(){
   
     headerMenu.style.width = "100vw";
     headerMenu.style.position = "absolute";
-    headerMenu.style.top = "-20px";
+  //  headerMenu.style.top = "-20px";
     headerMenu.style.left = "0";
     headerMenu.style.paddingRight = "20px";
-    headerMenu.style.backgroundColor = "#333333";
+    headerMenu.style.paddingBottom = "0";
+    headerMenu.style.background = "#333333";
     document.querySelector(".header-phone-viber-link").style.marginRight = "0";
     logoImg.classList.add("logo-img-wht");
     logoImg.classList.remove("logo-img-blc");
@@ -75,5 +81,33 @@ function closeMenu(){
     navBar.style.display = "flex";
     document.querySelector(".logo-text").style.color = "black";
     document.querySelector(".header-phone-viber-link").style.paddingRight = "12px";
-  
+    document.querySelector(".header-background").style.marginTop = "20px";
 }
+
+let dropdownMenu = document.getElementsByClassName("dropdown-list");
+let dropdownArrow = document.getElementsByClassName("dropdown-arrow");
+
+for (let i = 0; i < dropdownArrow.length; i++){
+    dropdownArrow[i].addEventListener('click', () => dropdownHandler(dropdownArrow[i], i));
+}
+
+function dropdownHandler(element, index){
+    
+        element.stopPropagation;
+        dropdownMenu[index].classList.toggle("list-visible");
+        element.classList.toggle("dropdown-open");
+        document.querySelectorAll(".dropdown-type")[index].classList.toggle("dropdown-opacity");
+    }
+ 
+   
+document.addEventListener("click", function(event){
+    for (let i = 0; i < dropdownArrow.length; i++){
+        if (event.target!=dropdownArrow[i] && 
+            dropdownMenu[i].classList.contains("list-visible")){
+            dropdownMenu[i].classList.toggle("list-visible");
+            dropdownArrow[i].classList.toggle("dropdown-open");
+            document.querySelectorAll(".dropdown-type")[index].classList.toggle("dropdown-opacity");
+        }
+    }
+    
+});
